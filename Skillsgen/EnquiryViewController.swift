7eef55e780e1431725fd7ee665fc2fef1f07e6f1
arc7722 <sreinolds@gmail.com>
2018-02-26 12:58:18 +0000

    email, phone fields are now interactable

--- Skillsgen/EnquiryViewController.swift
+++ Skillsgen/EnquiryViewController.swift
@@ -14,8 +14,8 @@ class EnquiryViewController: UIViewController {
     
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var phoneLabel: UILabel!
-    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UITextView!
+    @IBOutlet weak var emailLabel: UITextView!    
     @IBOutlet weak var enqLabel: UILabel!
     
     override func viewDidLoad() {
