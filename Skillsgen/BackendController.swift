00deba7b0bb80cff3e70ed358d2c67653dbf132a
arc7722 <sreinolds@gmail.com>
2018-03-06 10:49:49 +0000

    improvements

--- Skillsgen/BackendController.swift
+++ Skillsgen/BackendController.swift
@@ -46,10 +46,15 @@ class BackendController {
     }
     
     func fetchEnquiries(completion: @escaping (Bool) -> Void) {
+        var lastEnquiry = 0
+        if self.enquiries.count != 0 {
+            lastEnquiry = self.enquiries[0].id
+        }
         
         var components = URLComponents(url: Config.baseURL, resolvingAgainstBaseURL: true)!
         components.queryItems = [
             URLQueryItem(name: "query", value: "enquiries"),
+            URLQueryItem(name: "last", value: String(lastEnquiry)),
             URLQueryItem(name: Config.authQuery, value: Config.authValue)
         ]
         let url = components.url!
@@ -59,7 +64,7 @@ class BackendController {
             if let data = data,
                let enquiries = try? jsonDecoder.decode([Enquiry].self, from: data)
             {
-                self.enquiriesFile(enquiries)
+                self.syncEnquiries(enquiries)
                 completion(true)
             } else {
                 completion(false)
@@ -70,7 +75,7 @@ class BackendController {
     
     // Redo This! This is not robust, what if an enquiry is deleted? etc
     
-    func enquiriesFile(_ enquiriesFromServer: [Enquiry]) {
+    func syncEnquiries(_ enquiriesFromServer: [Enquiry]) {
         let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         let archiveURL = documentsDirectory.appendingPathComponent("enquiries").appendingPathExtension("plist")
         let propertyListDecoder = PropertyListDecoder()
@@ -79,12 +84,7 @@ class BackendController {
         if let retrievedEnquiries = try? Data(contentsOf: archiveURL),
            let decodedEnquiries = try? propertyListDecoder.decode(Array<Enquiry>.self, from: retrievedEnquiries)
         {
-            if(enquiriesFromServer.count > decodedEnquiries.count) {
-                let newEnquiriesCount = enquiriesFromServer.count - decodedEnquiries.count
-                newEnquiryList = Array(enquiriesFromServer.prefix(newEnquiriesCount)) + decodedEnquiries
-            } else {
-                newEnquiryList = decodedEnquiries
-            }
+            newEnquiryList = enquiriesFromServer + decodedEnquiries
         } else {
             newEnquiryList = enquiriesFromServer
         }
@@ -116,11 +116,8 @@ class BackendController {
     
     
     func resetEnquiries() {
-        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let archiveURL = documentsDirectory.appendingPathComponent("enquiries").appendingPathExtension("plist")
-        let propertyListEncoder = PropertyListEncoder()
+        self.enquiries = []
+        writeEnquiries()
         
-        let encodedEnquiries = try? propertyListEncoder.encode([])
-        try? encodedEnquiries?.write(to: archiveURL, options: .noFileProtection)
     }
 }
